499d75f334d99e2dd607753f9735a0526c4ae661
bparedes1993 <50050480+bparedes1993@users.noreply.github.com>
2026-09-25 21:54:17 -0500

    Habilitar eliminación propia y proteger respaldos por cuenta

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.BAP_CLOUD_CONFIG = {
   url: 'https://jytscsfyljyxvduymhpd.supabase.co',
-  publishableKey: 'sb_publishable_YCJwAFhQ1SL3Ql_FK_0vhw_KNCG_UrW'
+  publishableKey: 'sb_publishable_YCJwAFhQ1SL3Ql_FK_0vhw_KNCG_UrW',
+  accountDeletion: true
 };
